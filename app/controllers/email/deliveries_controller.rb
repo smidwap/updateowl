65e81fca248fc9bc57de947c4c50bdd4b4a701b9
Matt De Leon <matt@developertown.com>
2012-08-07 00:45:47 -0400

    Actually add checked to deliveries controller

--- app/controllers/email/deliveries_controller.rb
+++ app/controllers/email/deliveries_controller.rb
@@ -2,6 +2,7 @@ module Email
   class DeliveriesController < BaseController
     def show
       @delivery = Delivery.find_by_access_code(params[:access_code])
+      @delivery.checked!
     end
   end
 end
