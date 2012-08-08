439e378f1d1db0de6d63b9bcbf5e3d04df71de0a
Matt De Leon <matt@developertown.com>
2012-08-07 23:36:28 -0400

    Fix from address in mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,6 +2,6 @@ class MessageMailer < ActionMailer::Base
   def notification(delivery)
     @delivery = delivery
 
-    mail(to: @delivery.parent.email, from: "#{@delivery.message.student.grade_level.school.name} <no-reply@updatemeapp.com>")
+    mail(to: @delivery.parent.email, from: "#{@delivery.message.student.grade_level.school.name} <no-reply@update-me.developertown.com>")
   end
 end
