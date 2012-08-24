f7a7497b9b2375bc6f60f92b520f1a56e165c1eb
Matt De Leon <matt@developertown.com>
2012-08-24 01:14:10 -0400

    Track update checked only on first successful update

--- app/models/delivery_observer.rb
+++ app/models/delivery_observer.rb
@@ -1,11 +1,18 @@
 class DeliveryObserver < ActiveRecord::Observer
   def after_update(delivery)
-    if delivery.delivered_at_changed?
+    if should_track_check?(delivery)
       with_current_user(delivery.user) do
         track_event "Update: Checked by Parent", {
           "School" => delivery.try(:user).try(:school).try(:name)
         }
       end
     end
+  end
+
+  private
+
+  # Is this the first successful delivery of this message 
+  def should_track_check?(delivery)
+    delivery.delivered_at_changed? && delivery.message.deliveries.successful.count == 1
   end
 end
