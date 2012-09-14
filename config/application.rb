6b465af996e6d7601a380df86f263984d928bf75
Matt De Leon <matt@developertown.com>
2012-09-13 22:15:56 -0400

    Default to central time

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module UpdateMe
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -59,7 +59,5 @@ module UpdateMe
       g.test_framework :rspec, :views => false
       g.template_engine :haml
     end
-    
-    config.time_zone = "Eastern Time (US & Canada)"
   end
 end
