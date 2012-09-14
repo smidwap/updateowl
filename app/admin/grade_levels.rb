5b3ecff0a623a1778b0e1e45d339f65dd18d1f8f
Matt De Leon <matt@developertown.com>
2012-09-13 23:23:19 -0400

    School in grade levels admin

--- app/admin/grade_levels.rb
+++ app/admin/grade_levels.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register GradeLevel do
-  
+  index do
+    column :id, sortable: true
+    column :name, sortable: true
+    column :school do |grade_level|
+      grade_level.school.name
+    end
+    default_actions
+  end
 end
