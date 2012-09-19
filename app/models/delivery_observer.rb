470cf0191198ca1762457ac9d0b362b9c9e82b0e
Matt De Leon <matt@developertown.com>
2012-09-19 00:02:09 -0400

    Track translation of updates and whether a delivery to a parent has been translated

--- app/models/delivery_observer.rb
+++ app/models/delivery_observer.rb
@@ -3,7 +3,8 @@ class DeliveryObserver < ActiveRecord::Observer
     if should_track_check?(delivery)
       Analytics.with_current_user(delivery.user) do
         Analytics.track_event "Update: Checked by Parent", {
-          "School" => delivery.try(:user).try(:school).try(:name)
+          "School" => delivery.try(:user).try(:school).try(:name),
+          "Translated" => delivery.parent.spanish_speaking?
         }
       end
     end
--- app/models/message_observer.rb
+++ app/models/message_observer.rb
@@ -5,7 +5,8 @@ class MessageObserver < ActiveRecord::Observer
       "School" => message.school.name,
       "Grade Levels" => message.grade_levels.map(&:name),
       "Teacher" => message.user.full_name,
-      "Mass Update" => !message.individual?
+      "Mass Update" => !message.individual?,
+      "Translated" => message.requires_translation?
     }
   end
 end
