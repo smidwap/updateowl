05c061b9b7e11856dcc3351e140d695f08bcf4aa
Matt De Leon <matt@developertown.com>
2012-09-19 00:08:01 -0400

    Track all deliveries of messages. Forget about only tracking only the first delivery, because mass updates requires tracking all

--- app/models/delivery_observer.rb
+++ app/models/delivery_observer.rb
@@ -1,19 +1,10 @@
 class DeliveryObserver < ActiveRecord::Observer
   def after_update(delivery)
-    if should_track_check?(delivery)
-      Analytics.with_current_user(delivery.user) do
-        Analytics.track_event "Update: Checked by Parent", {
-          "School" => delivery.try(:user).try(:school).try(:name),
-          "Translated" => delivery.parent.spanish_speaking?
-        }
-      end
+    Analytics.with_current_user(delivery.user) do
+      Analytics.track_event "Update: Checked by Parent", {
+        "School" => delivery.try(:user).try(:school).try(:name),
+        "Translated" => delivery.parent.spanish_speaking?
+      }
     end
-  end
-
-  private
-
-  # Is this the first checked delivery of this message 
-  def should_track_check?(delivery)
-    delivery.checked_at_changed? && delivery.message.deliveries.checked.count == 1 #TODO: Is this complexity worth it?
   end
 end
