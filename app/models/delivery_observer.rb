999b59d2265dad4478b72e7226f344e3fd598118
Matt De Leon <matt@developertown.com>
2012-09-19 23:02:42 -0400

    Add via property to update checked by parent analytic

--- app/models/delivery_observer.rb
+++ app/models/delivery_observer.rb
@@ -3,7 +3,8 @@ class DeliveryObserver < ActiveRecord::Observer
     Analytics.with_current_user(delivery.user) do
       Analytics.track_event "Update: Checked by Parent", {
         "School" => delivery.try(:user).try(:school).try(:name),
-        "Translated" => delivery.parent.spanish_speaking?
+        "Translated" => delivery.parent.spanish_speaking?,
+        "Via" => delivery.parent.preference
       }
     end
   end
