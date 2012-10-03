b4190225578f3626d2835ee5c2e659bc887feb78
Matt De Leon <matt@developertown.com>
2012-10-03 01:15:49 -0400

    Consistency analytics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,23 @@ class UsersController < ApplicationController
       @consistency = Consistency.new(@user)
     end
   end
+
+private
+
+  after_filter :track_consistency, only: :consistency
+
+  def track_consistency
+    comparison_type = if params[:comparison_user_id]
+      'Coworker'
+    elsif params[:school_average_comaprison]
+      'School Average'
+    else
+      'None'
+    end
+
+    Analytics.track_event "Consistency: Compare", {
+      "School" => @user.school.name,
+      "Comparison Type" => comparison_type
+    }
+  end
 end
