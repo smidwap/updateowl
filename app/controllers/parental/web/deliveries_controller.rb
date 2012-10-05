0dea019dc792ad424e1addeaf1ae64c8ce2af8c4
Matt De Leon <matt@developertown.com>
2012-10-05 10:15:52 -0400

    Bring back show method for web-based deliveries to support emails previously sent out.

--- app/controllers/parental/web/deliveries_controller.rb
+++ app/controllers/parental/web/deliveries_controller.rb
@@ -8,9 +8,24 @@ module Parental
         I18n.locale = @deliveries.first.parent.locale
       end
 
+      def show
+        @delivery = Delivery.find_by_access_code(params[:id])
+        @delivery.checked!
+
+        I18n.locale = @delivery.parent.locale
+      end
+
       def feedback
         @delivery = Delivery.find_by_access_code(params[:id])
 
+        head :ok
+      end
+
+    private
+
+      after_filter :track_feedback, only: :feedback
+
+      def track_feedback
         Analytics.track_parent_event @delivery.parent, "Update: Poor Translation", {
           "Message ID" => @delivery.message_id
         }
