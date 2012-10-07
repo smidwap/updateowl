b20ca96d6d0a7c8a3835726c96f5e46829016ab4
Matt De Leon <matt@developertown.com>
2012-10-07 11:53:59 -0400

    Add more stats for users in active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -9,14 +9,21 @@ ActiveAdmin.register User do
   end
 
   show do
-    students_with_no_updates = user.students.with_registered_parents.select { |student| student.messages.individual.length == 0 }
+    students_with_registered_parents = user.students.with_registered_parents.all
+    students_with_no_updates = students_with_registered_parents.select { |student| student.messages.individual.length == 0 }
     attributes_table do
       row :id
       row :first_name
       row :last_name
       row :email
+      row "number of students with registered parents" do |user|
+        students_with_registered_parents.length
+      end
       row "number of students without updates" do |user|
         students_with_no_updates.length
+      end
+      row "percentage" do |user|
+        (students_with_no_updates.length.to_f / students_with_registered_parents.length * 100).round if students_with_registered_parents.length > 0
       end
       row "students without updates" do |user|
         students_with_no_updates.sort_by(&:last_name).map(&:full_name).join("<br>").html_safe
