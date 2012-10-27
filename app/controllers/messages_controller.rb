0041a66aa62501ac53dfeae7c3d598bdffbb4b02
Matt De Leon <matt@developertown.com>
2012-10-27 15:27:15 -0400

    Track when updates are sent from the extension

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,7 +14,21 @@ class MessagesController < ApplicationController
 
     @message.save!
     @message.reload
+
+    track_create_if_sent_from_extension
   rescue ActiveRecord::RecordInvalid
     render_resource_invalid @message
+  end
+
+private
+
+  def track_create_if_sent_from_extension
+    if params[:format] == 'extension'
+      Analytics.track_event 'Update: Sent from Extension', {
+        "School" => @message.school.name,
+        "Grade Levels" => @message.grade_levels.map(&:name),
+        "Teacher" => @message.user.full_name,
+      }
+    end
   end
 end
