fa33da15c4a72be561fc0776828f7a59bb9868f2
Matt De Leon <matt@developertown.com>
2012-11-11 12:24:16 -0500

    Configure redis port on production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis.new
+
+redis_config_path = File.expand_path(File.dirname(__FILE__) + "/../redis.yml")
+redis_cfg = YAML.load(File.read(redis_config_path))[Rails.env]
+
+$redis = Redis.new(host: redis_cfg['host'], port: redis_cfg['port'])
