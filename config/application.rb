f8eb4c759ba9b5b4ec40c5acbd2387d657890971
Matt De Leon <matt@developertown.com>
2012-11-11 02:23:38 -0500

    Don't rely on app to be initialized for asset precomp

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module UpdateMe
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Don't initialize the app when running rake assets:precompile
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.generators do |g|
