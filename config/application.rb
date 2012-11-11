9e6bfd698bcc48342b34de31c33a4a7f4bea19b8
Matt De Leon <matt@developertown.com>
2012-11-11 13:08:54 -0500

    Move precompilation list to app config, and add acive admin assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,10 @@ module UpdateMe
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+    config.assets.precompile += %w( active_admin.css, active_admin.js, extension/application.js, extension/application.css )
+
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
