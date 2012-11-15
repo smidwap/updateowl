e758a40044d44e03bec1e141d2b0e0f40e499f41
Matt De Leon <matt@developertown.com>
2012-11-15 00:19:19 -0500

    Fix callback url host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ UpdateMe::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # For twilio callbacks
-  config.root_url = 'http://' + config.action_mailer.default_url_options[:host]
+  config.root_url = 'http://www.updateowl.com'
 
   # Google analytics
   GA.tracker = 'UA-34778894-1'
