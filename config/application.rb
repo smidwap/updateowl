f8a9b7284367066437efb01bff00bfdd374c7c44
Matt De Leon <matt@developertown.com>
2012-11-26 15:51:13 -0500

    Check to see if application.css and application.js need to be included in precompile list

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module UpdateMe
     config.active_record.whitelist_attributes = true
 
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-    config.assets.precompile += %w( active_admin.css active_admin.js extension/application.js extension/application.css marketing/style.css marketing/marketing.js )
+    config.assets.precompile += %w( application.css application.js active_admin.css active_admin.js extension/application.js extension/application.css marketing/style.css marketing/marketing.js )
 
     # Enable the asset pipeline
     config.assets.enabled = true
