ed3451ac2363b28a09ba6ecc8566ff2432f1ed27
Matt De Leon <matt@developertown.com>
2012-12-04 15:04:08 -0500

    Change default url for emails to www.updateowl.com. Hoping this fixes an issue with checking updates via email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ UpdateMe::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :ses
-  config.action_mailer.default_url_options = { :host => 'updateowl.com' }
+  config.action_mailer.default_url_options = { :host => 'www.updateowl.com' }
   config.action_mailer.smtp_settings = {
     :openssl_verify_mode => 'none'
   }
