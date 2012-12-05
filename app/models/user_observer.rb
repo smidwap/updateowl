7a8eef70bb7c2427d619d927b9a3210acec0e305
Matt De Leon <matt@developertown.com>
2012-12-04 22:05:21 -0500

    Get rid of unneeded call to mixpanel people api that may be causing a massive buildup in the queue

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -10,10 +10,6 @@ class UserObserver < ActiveRecord::Observer
     if user.last_sign_in_at_changed?
       Analytics.with_current_user(user) do
         Analytics.track_event "User: Sign In"
-
-        Analytics.set_people_properties({
-          "$last_login" => Time.now
-        })
       end
     end
   end
