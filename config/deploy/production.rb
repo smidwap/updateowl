eba8ff3663dfad26732dca263317bd2aa5422a04
Matt De Leon <smidwap@gmail.com>
2013-03-17 01:15:23 -0400

    Get rid of deployment scripts thsat aren't used

--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-set :branch, 'master'
-set :rails_env, 'production'
-
-# CONFIGURE THIS
-role :web, "updateowl-prodweb01.devtown.int"                          # Your HTTP server, Apache/etc
-# CONFIGURE THIS
-role :app, "updateowl-prodweb01.devtown.int"                          # This may be the same as your `Web` server
-# CONFIGURE THIS
-role :db,  "updateowl-prodweb01.devtown.int", :primary => true        # This is where Rails migrations will run
